dd50c2dc7b4f4a00304c555feea677bdff2caf76
Pranil Kakade <pranilkakade111@gmail.com>
2021-04-14 00:00:27 +0530

    Adding code to check attendance of employee

--- Employee.js
+++ Employee.js
@@ -1,1 +1,27 @@
 console.log("Welcome To Employee Wage Program");
+var empcheck=0;
+var present=1;
+
+class EmployeeWage
+{
+    constructor(){}
+
+    employeeAttendence()
+    {
+        empcheck=Math.floor(Math.random() *10 % 2);
+        if(empcheck==present)
+        {
+            return "Employee is Present";
+        }
+        else
+        {
+            return "Employee is Abscent";
+        }
+    }
+}
+let display=function()
+{
+    let employeeWage = new EmployeeWage().employeeAttendence();
+    console.log(employeeWage);
+}
+display();
