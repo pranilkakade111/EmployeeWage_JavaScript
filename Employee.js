d0ed6b670681e8ea981ee6b35fa92ce504ec3ca0
Pranil Kakade <pranilkakade111@gmail.com>
2021-04-14 11:09:34 +0530

    Adding code to check part time of employee and calculate wage

--- Employee.js
+++ Employee.js
@@ -1,6 +1,14 @@
 console.log("Welcome To Employee Wage Program");
 var empcheck=0;
+let attendance;
 var present=1;
+let salary=0;
+const IS_PART_TIME = 2;
+const EMP_RATE_PER_HR = 20;
+const NUM_OF_WORKING_DAYS = 20;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const MAX_HRS_IN_MONTH = 100;
 
 class EmployeeWage
 {
@@ -8,20 +16,46 @@ class EmployeeWage
 
     employeeAttendence()
     {
-        empcheck=Math.floor(Math.random() *10 % 2);
+        empcheck=Math.floor(Math.random() *10 % 3);
         if(empcheck==present)
         {
+            attendance="present";
             return "Employee is Present";
+        }
+        else if(empcheck==IS_PART_TIME)
+        {
+            attendance="part-time";
+           return "Employee is Part-Time Present!!!";
         }
         else
         {
+            attendance="abscent";
             return "Employee is Abscent";
         }
     }
-}
+    dailyWage(attendance)
+    {
+        if(empcheck==present)
+        {
+            salary = EMP_RATE_PER_HR * FULL_TIME_HOURS;
+        }
+        else if(empcheck==IS_PART_TIME)
+        {
+            salary = EMP_RATE_PER_HR * PART_TIME_HOURS;
+        }
+        else
+        {
+            salary = EMP_RATE_PER_HR * 0;
+        }
+        console.log("Daily Wage:"+salary)
+        return salary;
+    }
+ }
 let display=function()
 {
     let employeeWage = new EmployeeWage().employeeAttendence();
     console.log(employeeWage);
+    let employee_Wage = new EmployeeWage().dailyWage();
+    console.log(employee_Wage);
 }
 display();
